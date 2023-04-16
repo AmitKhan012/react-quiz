710af86aec1c7af073d4781ce7d7d23f075eeab6
AmitKhan012 <uchihaamit1@gmail.com>
2023-04-16 16:43:02 +0600

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
+import "../styles/App.css";
+import Layout from "./Layout";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route exact path="/signup" element={<PublicRoute />}>
+              <Route path="/signup" element={<Signup />} />
+            </Route>
+            <Route exact path="/login" element={<PublicRoute />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+            <Route exact path="/quiz/:id" element={<PrivateRoute />}>
+              <Route path="/quiz/:id" element={<Quiz />} />
+            </Route>
+            <Route exact path="/result/:id" element={<PrivateRoute />}>
+              <Route path="/result/:id" element={<Result />} />
+            </Route>
+          </Routes>
+        </Layout>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
